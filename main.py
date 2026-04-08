18368bb313628c20c136d1bea0c5234c813362f2
Matvey Putilin <shumovaputilin@mail.ru>
2026-04-08 21:31:52 +0300

    Create env and gitognore >> env

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('Hello from responsitory!')
+from dotenv import load_dotenv
+import os
+
+def print_author():
+    load_dotenv()
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
+
+# Вызываем функцию — теперь код внутри неё выполнится
+print_author()
